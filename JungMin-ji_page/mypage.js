d3e6f3c08b0c3defd0e34bdb9baff1bf02d76606
Jung-Minji <mjjunghp@naver.com>
2020-12-24 04:04:38 +0900

    finish head

--- JungMin-ji_page/mypage.js
+++ JungMin-ji_page/mypage.js
@@ -9,3 +9,26 @@ $(function(){
 		$('.gnb .inner').removeClass('on');
 	});
 });
+
+
+// 스크롤 시 header 높이 줄이기
+
+// fixheader
+var scrollFix = 0;
+scrollFix = $(document).scrollTop();
+fixHeader();
+
+// 윈도우 창 조절 시 이벤트 
+$(window).on('scroll resize', function(){
+	scrollFix = $(document).scrollTop();
+	fixHeader();
+});
+
+// fixheader함수
+function fixHeader(){
+	if(scrollFix > 150){
+		$('header').addClass('on');
+	}else{
+		$('header').removeClass('on');
+	}
+}
