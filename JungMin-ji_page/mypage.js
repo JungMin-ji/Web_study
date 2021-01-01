1de458d7cc17b4b7d3cedf8840305ef5634e9758
Jung-Minji <mjjunghp@naver.com>
2021-01-02 01:50:00 +0900

    add arrow, dot, text-animation

--- JungMin-ji_page/mypage.js
+++ JungMin-ji_page/mypage.js
@@ -32,3 +32,24 @@ function fixHeader(){
 		$('header').removeClass('on');
 	}
 }
+
+// 글자 애니메이션 
+$(function(){
+	Splitting();
+});
+
+// top-visual 이미지 슬라이드
+$(function(){
+	$('.visual .slide').slick({
+		arrows:true,	// 이미지 옆으로 넘기는 화살표
+		dots:true,		// 현재 이미지 위치 학인하는 dot 
+		autoplay:true,	// 이미지 자동으로 넘어감
+		fade:true,		// 페이드인 효과
+		autoplaySpeed:3000,		// 이미지 넘어가는 시간(3s)
+		pauseOnHover:false,		// 호버시 멈추는 것을 해제
+		pauseOnFocus:false
+
+	});
+
+	$('.slick-prev').text("prev");	// 사진 슬라이드 화살표 previous를 prev로 바꿈
+});
