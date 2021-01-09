92eeeb5dd1a336ef5a2a7cee056a5970761c447a
Jung-Minji <mjjunghp@naver.com>
2021-01-10 02:56:20 +0900

    complete entire structure of project page, todo : 1. fix header of project page, entire page, 2. change tech stack color of project page 3. change font of project page 4. change each img of project”

---
+++ JungMin-ji_page/js/project.js
@@ -0,0 +1,9 @@
+// $(document).ready( function matchColor(){
+// 	var val = document.getElementById('stack').innerHTML;
+// 	if (val == "Python"){
+// 	document.getElementById('stack').style.backgroundColor = "black";
+// 	}
+// 	else if (val == "Ros"){
+// 		val.style.backgroundColor = "blue";
+// 	}
+// });
