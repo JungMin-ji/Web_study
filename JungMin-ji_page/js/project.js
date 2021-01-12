2c8639e03a47e5ef01689bbba75f58365453fbb4
Jung-Minji <mjjunghp@naver.com>
2021-01-12 22:28:28 +0900

    add img

--- JungMin-ji_page/js/project.js
+++ JungMin-ji_page/js/project.js
@@ -1,9 +1,15 @@
-// $(document).ready( function matchColor(){
-// 	var val = document.getElementById('stack').innerHTML;
-// 	if (val == "Python"){
-// 	document.getElementById('stack').style.backgroundColor = "black";
-// 	}
-// 	else if (val == "Ros"){
-// 		val.style.backgroundColor = "blue";
-// 	}
-// });
+$(function(){
+
+$('.slide').slick({
+		arrows:false,	// 이미지 옆으로 넘기는 화살표
+		dots:true,		// 현재 이미지 위치 학인하는 dot 
+		autoplay:false,	// 이미지 자동으로 넘어감
+		pauseOnHover:true,		// 호버시 멈추도록
+		pauseOnFocus:true,
+		slidesToShow:1,
+		slidesToScroll:1
+	});
+// $('.slide #slick-slide-control00').text("");
+// $('.slide #slick-slide-control01').text("");
+
+});
