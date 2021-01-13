8137852dca6f639d6b4b42529659c13295f86e62
Jung-Minji <mjjunghp@naver.com>
2021-01-13 22:48:30 +0900

    complete project page's img

--- JungMin-ji_page/js/project.js
+++ JungMin-ji_page/js/project.js
@@ -1,7 +1,7 @@
 $(function(){
 
 $('.slide').slick({
-		arrows:false,	// 이미지 옆으로 넘기는 화살표
+		arrows:true,	// 이미지 옆으로 넘기는 화살표
 		dots:true,		// 현재 이미지 위치 학인하는 dot 
 		autoplay:false,	// 이미지 자동으로 넘어감
 		pauseOnHover:true,		// 호버시 멈추도록
@@ -9,7 +9,7 @@ $('.slide').slick({
 		slidesToShow:1,
 		slidesToScroll:1
 	});
-// $('.slide #slick-slide-control00').text("");
-// $('.slide #slick-slide-control01').text("");
+$('.slick-prev').text("");
+$('.slick-next').text("");
 
 });
