5ba495b96be10c60f205f0c06449833631d22de3
Jung-Minji <mjjunghp@naver.com>
2021-01-19 22:03:30 +0900

    complete page of contest

--- JungMin-ji_page/js/project.js
+++ JungMin-ji_page/js/project.js
@@ -1,3 +1,37 @@
+//gnb
+$(function(){
+	$('.gnb > li > a').on('mouseenter focus', function(){
+		var gnbidx = $('.gnb > li > a').index($(this));
+		$('.gnb .inner').removeClass('on');
+		$('.gnb .inner:eq('+gnbidx+')').addClass('on');
+	});
+	$('header').on('mouseleave', function(){
+		$('.gnb .inner').removeClass('on');
+	});
+});
+
+
+// 스크롤 시 header 높이 줄이기
+// fixheader
+var scrollFix = 0;
+scrollFix = $(document).scrollTop();
+fixHeader();
+
+// 윈도우 창 조절 시 이벤트 
+$(window).on('scroll resize', function(){
+	scrollFix = $(document).scrollTop();
+	fixHeader();
+});
+
+// fixheader함수
+function fixHeader(){
+	if(scrollFix > 150){
+		$('header').addClass('on');
+	}else{
+		$('header').removeClass('on');
+	}
+}
+
 $(function(){
 
 $('.slide').slick({
